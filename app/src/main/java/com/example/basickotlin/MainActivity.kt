841bbfa11c1200cc5aef8b35e4e4340cfdc9a8f9
Jorge Rodriguez <jorge-11@live.com.ar>
2021-07-10 02:00:47 -0300

    TypeOfData

--- app/src/main/java/com/example/basickotlin/MainActivity.kt
+++ app/src/main/java/com/example/basickotlin/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        variablesAndConstants()
+        //variablesAndConstants()
+        typeOfData()
     }
 
     /* example comment */
@@ -33,5 +34,34 @@ class MainActivity : AppCompatActivity() {
 
         println( myConstant)
         println(mySecondConstant)
+    }
+
+    //Type of Data
+    private fun typeOfData(){
+        //String
+        val myString = "My String"
+        val myString2 = "My Second String"
+        val myString3 = myString + " " + myString2
+        println(myString3)
+
+        //Int (Byte, Short, Int, Long)
+        val myInt = 1
+        val myInt2 = 2
+        val myInt3 = myInt + myInt2
+        println(myInt3)
+
+        //Decimal (Float, Double)
+        val myFloat : Float = 1.5f
+        val myDouble = 1.5
+        val myDouble2 = 2.6
+        val myDouble3 = 1
+        val myDouble4 = myDouble + myDouble2 + myDouble3
+        println(myDouble4)
+
+        //Boolean (Bool)
+        val myBool = true
+        val myBool2 = false
+        println(myBool == myBool2)
+        println(myBool && myBool2)
     }
 }
