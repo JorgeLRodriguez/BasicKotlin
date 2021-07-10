ca63011975766e5786a0aa18e7033e94534ba50b
Jorge Rodriguez <jorge-11@live.com.ar>
2021-07-10 02:34:31 -0300

    if sentence

--- app/src/main/java/com/example/basickotlin/MainActivity.kt
+++ app/src/main/java/com/example/basickotlin/MainActivity.kt
@@ -8,11 +8,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //variablesAndConstants()
-        typeOfData()
+        //typeOfData()
+        ifStatement()
     }
 
     /* example comment */
 
+    //Variables and Constants
     private fun variablesAndConstants(){
         //variables
 
@@ -63,5 +65,49 @@ class MainActivity : AppCompatActivity() {
         val myBool2 = false
         println(myBool == myBool2)
         println(myBool && myBool2)
+    }
+
+    //If
+    private fun ifStatement(){
+        val myNumber = 60
+
+        /*
+        Conditional operators
+        > greater than
+        < less than
+        >= greater or equal than
+        <= less or equal than
+        = equal than
+        != different than
+         */
+
+        if(myNumber < 10){
+            println("$myNumber is less than 10")
+        }
+        else{
+            println("$myNumber is greater than 10")
+        }
+
+        /*
+        Logical operators
+        && "and" operator
+        || "or" operator (Alt + 124)
+        ! "no" operator
+         */
+
+        if (!(myNumber in 6..9 || myNumber == 53)){
+            println("$myNumber is less than or equal to 10 and greater than 5 or equals 53")
+        }
+        else if(myNumber == 60){
+            println("$myNumber equals 60")
+        }
+        else if(myNumber != 70){
+            println("$myNumber is not the same as 70")
+        }
+        else {
+
+            println("$myNumber es greater than 10 or less or equals 5 and is not the same as 53")
+        }
+
     }
 }
